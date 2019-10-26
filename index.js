f71d1c279b1050965384c56070157e714f017b58
Danny Lee <dreamer@panix.com>
2019-10-26 19:20:53 -0400

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Write your solution in this file!
 customerName = "bob";
-const leastFavoriteCustomer = "bab";
+CONST leastFavoriteCustomer = "bab";
 
 function upperCaseCustomerName() {
     customerName = customerName.toUpperCase();
